aedc848eb587348ca46d5d59c2304f863afba625
Timur Mahmudov <verloren454@gmail.com>
2024-10-03 08:15:06 +0300

    Create quiz questions

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_TOKEN = os.getenv("API_TOKEN")
+DB_NAME = os.getenv("DB_NAME")
+
---
+++ quiz_questions.py
@@ -0,0 +1,58 @@
+quiz_data = [
+    {
+        "question": "Что такое Python?",
+        "options": ["Язык программирования",
+                    "Тип данных",
+                    "Музыкальный инструмент",
+                    "Змея по английски"],
+        "correct_option": 0
+    },
+    {
+        "question": "Какой тип данных используется для хранения целых чисел",
+        "options": ["int", "float", "str", "natural"],
+        "correct_option": 0
+    },
+    {
+        "question": "Как строку можно преобразовать в список?",
+        "options": ["tuple",
+                    "list",
+                    "set",
+                    "Нельзя строку преобразовать в список"],
+        "correct_option": 1
+    },
+    {
+        "question": "С помощью чего создаются именованные функции?",
+        "options": ["const", "func", "function", "def", "Нет правильного ответа"],
+        "correct_option": 3
+    },
+    {
+        "question": "К какому типу данных относится: True",
+        "options": ["str", "float", "bool", "tuple"],
+        "correct_option": 2
+    },
+    {
+        "question": "Результат выражения: '4' * 5", 
+        "options": [20, '20', "TypeError", '44444'],
+        "correct_option": 3
+    },
+    {
+        "question": "С помощью какого метода можно из списка строк сделать 1 строку?",
+        "options": ["str", "join", "split"],
+        "correct_option": 1
+    },
+    {
+        "question": "Что вернёт метод get у словаря?",
+        "options": [-1, None, KeyError],
+        "correct_option": 1
+    },
+    {
+        "question": "",
+        "options": [],
+        "correct_option":
+    },
+    {
+        "question": "",
+        "options": [],
+        "correct_option":
+    },
+]
