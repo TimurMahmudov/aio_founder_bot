2672e89987b67b96f5bee55eee1d330137a1bafc
Timur Mahmudov <verloren454@gmail.com>
2024-10-09 10:36:16 +0300

    add routing

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
-import os
 import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters.command import Command
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
-import nest_asyncio
 
-from .create_db_responses import create_table
+from create_db_responses import create_table
+from quiz import new_quiz
+from config import API_TOKEN
+from callbacks_answers import router
 
-nest_asyncio.apply()
+# nest_asyncio.apply()
 
 logging.basicConfig(level=logging.INFO)
 
-await create_table()
-
-API_TOKEN = os.getenv("API_TOKEN")
 
 bot = Bot(token=API_TOKEN)
 
 dp = Dispatcher()
 
 
-@dp.message(Command(["start"]))
+@dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     builder = ReplyKeyboardBuilder()
     builder.add(types.KeyboardButton(text="Начать игру"))
@@ -33,13 +31,16 @@ async def cmd_start(message: types.Message):
 
 
 @dp.message(F.text=="Начать игру")
-@dp.message(Command(["quiz"]))
+@dp.message(Command("quiz"))
 async def cmd_quiz(message: types.Message):
     await message.answer("Давайте начнём игру quiz!")
     await new_quiz(message)
 
 
 async def main():
+    
+    await create_table()
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 
