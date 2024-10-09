bce7afab2a505e03bbedbd76819b96d3f840457b
Timur Mahmudov <verloren454@gmail.com>
2024-10-09 10:35:11 +0300

    update

--- create_db_responses.py
+++ create_db_responses.py
@@ -1,14 +1,14 @@
 import aiosqlite
 
-from .config import DB_NAME
+from config import DB_NAME
 async def create_table():
     async with aiosqlite.connect("quiz_bot.db") as db:
         await db.execute(
-            '''CREATE TABLE IF NOT EXISTS {} (
+            '''CREATE TABLE IF NOT EXISTS quiz_state (
                 user_id INTEGER PRIMARY KEY,
                 question_index INTEGER,
                 right_answers INTEGER)
-            '''.format(DB_NAME)
+            '''
         )
 
         await db.commit()
